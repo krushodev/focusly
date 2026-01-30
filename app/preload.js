5e2494d7543c7ad5e3986f67ee698d2579e9ee5f
Ignacio Kruchowski <ignakruchowski@gmail.com>
2026-01-29 21:47:37 -0300

    first commit

---
+++ app/preload.js
@@ -0,0 +1,5 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  // Add any APIs you want to expose to the renderer process here
+});
